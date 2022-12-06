2aecad27917d370a30019fc2c9125dffc019bc7e
Vadim-AM <zayaz2805@gmai.com>
2022-12-06 14:32:44 +0300

    remote webdriver

--- conftest.py
+++ conftest.py
@@ -18,11 +18,11 @@ def browser(request):
         case "chrome":
             options = webdriver.ChromeOptions()
             options.headless = headless
-            browser = webdriver.Remote(options=options)
+            browser = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', options=options)
         case "firefox":
             options = webdriver.FirefoxOptions()
             options.headless = headless
-            browser = webdriver.Remote(options=options)
+            browser = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', options=options)
     request.cls.driver = browser
     browser.maximize_window()
     yield browser
