1e4b367f49d6aa4194bb7c384c2fa235f4821e6a
Vadim-AM <zayaz2805@gmail.com>
2022-12-10 22:28:56 +0300

    add firefox tests

--- conftest.py
+++ conftest.py
@@ -8,44 +8,44 @@ from webdriver_manager.firefox import GeckoDriverManager
 from ui_tests.pages.data import FormData
 
 
-@pytest.fixture()
-def browser():
-    """
-    the fixture downloads the latest driver and creates the browser instance with passed options
-    """
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    browser.maximize_window()
-    yield browser
-    browser.quit()
-
-
-# @pytest.fixture(params=["chrome", "firefox"])
-# def browser(request):
+# @pytest.fixture()
+# def browser():
 #     """
 #     the fixture downloads the latest driver and creates the browser instance with passed options
 #     """
-#     headless = False  # changes the headless parameter for all browsers
-#     match request.param:
-#         case "chrome":
-#             options = webdriver.ChromeOptions()
-#             options.headless = headless
-#             options.add_argument('--no-sandbox')
-#             options.add_argument("--disable-dev-shm-usage")
-#             browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-#         case "firefox":
-#             options = webdriver.FirefoxOptions()
-#             options.headless = headless
-#             options.add_argument('--no-sandbox')
-#             options.add_argument("--disable-dev-shm-usage")
-#             browser = webdriver.Firefox(GeckoDriverManager().install(), options=options)
-#     request.cls.driver = browser
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--headless")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--no-sandbox")
+#     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 #     browser.maximize_window()
 #     yield browser
 #     browser.quit()
+
+
+@pytest.fixture(params=["chrome", "firefox"])
+def browser(request):
+    """
+    the fixture downloads the latest driver and creates the browser instance with passed options
+    """
+    headless = False  # changes the headless parameter for all browsers
+    match request.param:
+        case "chrome":
+            options = webdriver.ChromeOptions()
+            options.headless = headless
+            options.add_argument('--no-sandbox')
+            options.add_argument("--disable-dev-shm-usage")
+            browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        case "firefox":
+            options = webdriver.FirefoxOptions()
+            options.headless = headless
+            options.add_argument('--no-sandbox')
+            options.add_argument("--disable-dev-shm-usage")
+            browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+    request.cls.driver = browser
+    browser.maximize_window()
+    yield browser
+    browser.quit()
 
 
 @pytest.fixture()
